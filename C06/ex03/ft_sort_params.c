f4d5117d5a2e125ab414a9eebebb2457b181a27d
BishopVK <danielgraindorge@gmail.com>
2023-12-03 01:36:09 +0100

    Creado el directorio C06 con las subcarpetas de los ejercicios del ex00 al ex03 todo correcto

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/28 17:46:10 by danjimen          #+#    #+#             */
+/*   Updated: 2023/11/28 18:16:34 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_sort_str_tab(char **tab, int size)
+{
+	char	*temp;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - i - 1)
+		{
+			if (ft_strcmp(tab[j], tab[j + 1]) > 0)
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	if (argc > 1)
+	{
+		ft_sort_str_tab(argv + 1, argc - 1);
+		i = 1;
+		while (i < argc)
+		{
+			write (1, argv[i], ft_strlen(argv[i]));
+			write (1, "\n", 1);
+			i++;
+		}
+	}
+	return (0);
+}
