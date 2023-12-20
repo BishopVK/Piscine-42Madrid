4cb4b6320c50cc5d5f01bb347936c37baa9739bc
BishopVK <danielgraindorge@gmail.com>
2023-12-20 19:53:23 +0100

    Corregido ejercicio ex03 del proyecto C07, pasada la norminette y compila correctamente

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bishopvk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 15:02:06 by bishopvk          #+#    #+#             */
-/*   Updated: 2023/12/20 12:31:45 by bishopvk         ###   ########.fr       */
+/*   Updated: 2023/12/20 19:45:58 by bishopvk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,8 @@ int	ft_strlen(int size, char **strs, char *sep)
 	while (i < size && strs[i])
 	{
 		j = 0;
-		while (strs[i][j])
-		{
-			fprintf(stderr, "%c %d %d\n", strs[i][j], i, j);
+		while (strs[i][j++])
 			count++;
-			j++;
-		}
 		i++;
 	}
 	i = 0;
@@ -37,7 +33,6 @@ int	ft_strlen(int size, char **strs, char *sep)
 	{
 		while (sep[i])
 		{
-			fprintf(stderr, "sep[i], count and i: %c %d %d\n", sep[i], count, i);
 			count += size - 1;
 			i++;
 		}
@@ -45,28 +40,13 @@ int	ft_strlen(int size, char **strs, char *sep)
 	return (count);
 }
 
-char	*ft_strjoin(int size, char **strs, char *sep)
+char	*ft_concat(int size, char *str, char *sep, char **strs)
 {
-	char	*str;
-	int		i;
-	int		j;
-	int		k;
-	int		l;
-	int		count;
+	int	i;
+	int	j;
+	int	k;
+	int	l;
 
-	count = ft_strlen(size, strs, sep);
-	printf("Count: %i\n", count);
-	if (size == 0)
-	{
-		str = (char *)malloc(1);
-		if (str == NULL)
-			exit (EXIT_FAILURE);
-		str[0] = '\0';
-		return (str);
-	}
-	str = (char *)malloc((count + 1) * sizeof(char));
-	if (str == NULL)
-		exit (EXIT_FAILURE);
 	i = 0;
 	j = 0;
 	while (j < size && strs[j])
@@ -86,14 +66,35 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	return (str);
 }
 
-int	main(void)
+char	*ft_strjoin(int size, char **strs, char *sep)
 {
-	int		size = 3;
-	char	*strs[] = {"Hola,", "soy", "Daniel"};
+	char	*str;
+	int		count;
+
+	count = ft_strlen(size, strs, sep);
+	if (size == 0)
+	{
+		str = (char *)malloc(1);
+		if (str == NULL)
+			exit (EXIT_FAILURE);
+		str[0] = '\0';
+		return (str);
+	}
+	str = (char *)malloc((count + 1) * sizeof(char));
+	if (str == NULL)
+		exit (EXIT_FAILURE);
+	str = ft_concat(size, str, sep, strs);
+	return (str);
+}
+
+/*int	main(void)
+{
+	int		size = 4;
+	char	*strs[] = {"Hola,", "soy", "Daniel", "Jiménez"};
 	char	*sep = "-*";
 	char	*resultado = ft_strjoin(size, strs, sep);
 
 	printf("Resultado: %s\n", resultado);
 	free(resultado);
 	return (0);
-}
+}*/
