46e855ac1d062c113acd4b9051e54711cb5ba3c6
BishopVK <danielgraindorge@gmail.com>
2024-01-08 17:50:41 +0100

    Creación del fichero ft_abs.h

--- C08/ex02/ft_abs.h
+++ C08/ex02/ft_abs.h
@@ -1,34 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_abs.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: danjimen <danjimen@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/21 13:21:53 by danjimen          #+#    #+#             */
-/*   Updated: 2023/12/22 17:48:51 by danjimen         ###   ########.fr       */
+/*   Created: 2024/01/03 12:17:41 by danjimen          #+#    #+#             */
+/*   Updated: 2024/01/03 12:21:40 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_boolean.h"
+// ft_abs.h
 
-void	ft_putstr(char *str)
-{
-	while (*str)
-		write(1, str++, 1);
-}
+#ifndef FT_ABS_H
+# define FT_ABS_H
 
-t_bool	ft_is_even(int nbr)
-{
-	return ((EVEN(nbr)) ? TRUE : FALSE);
-}
+//# define ABS(x) ((x) < 0 ? -(x) : (x))
+# define ABS(x) (((x) < 0) * -(x) + ((x) >= 0) * (x))
 
-int	main(int argc, char **argv)
-{
-	(void)argv;
-	if (ft_is_even(argc - 1) == TRUE)
-		ft_putstr(EVEN_MSG);
-	else
-		ft_putstr(ODD_MSG);
-	return (SUCCESS);
-}
+#endif /* FT_ABS_H */
